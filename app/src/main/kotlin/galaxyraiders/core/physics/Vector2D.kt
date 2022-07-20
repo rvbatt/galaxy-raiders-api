e7a7a6f7db396bab69d40e832773a16090634e13
Rodrigo Volpe Battistin <62215634+rvbatt@users.noreply.github.com>
2022-07-20 11:33:36 -0300

    Implement vector 'vector project' onto vector
    Co-authored-by: Johnny da Silva Lima <johnny.lima@usp.br>
    Co-authored-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -59,7 +59,7 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return (this * target) / target.magnitude * (target.unit)
   }
 }
 
