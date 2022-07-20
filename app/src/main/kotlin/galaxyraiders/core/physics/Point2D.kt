9cb50185ad4b222e06a3ef972ed6a7692f0bbede
Rodrigo Volpe Battistin <62215634+rvbatt@users.noreply.github.com>
2022-07-20 09:22:04 -0300

    Implement impact direction (normalized)
    Co-authored-by: Johnny da Silva Lima <johnny.lima@usp.br>
    Co-authored-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -22,7 +22,7 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactVector(p).normal
   }
 
   fun contactVector(p: Point2D): Vector2D {
